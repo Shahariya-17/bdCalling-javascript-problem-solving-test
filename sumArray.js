e72709d334d03c44f35e552350d3bf21d3286457
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-10-15 08:51:19 +0600

    added sum array

---
+++ sumArray.js
@@ -0,0 +1,16 @@
+// 4. Sum of Array Elements (5 Marks)
+// Write a function sumArray(numbers) that takes an array of numbers and returns the sum of all the numbers in the array.
+// Example:
+// Input: sumArray([1, 2, 3, 4])
+// Output: 10
+
+function sumArray(numbers){
+    let sum = 0;
+
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4]));
