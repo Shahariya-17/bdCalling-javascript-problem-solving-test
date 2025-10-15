f62410e1574a774ec1f315e50ecf1b1dd41d0ae7
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-10-15 08:39:59 +0600

    added evenOrOdd

---
+++ evenOrOdd.js
@@ -0,0 +1,17 @@
+// 2. Even or Odd (5 Marks)
+// Write a function isEvenOrOdd(number) that takes an integer and returns the string "Even" if the number is even, and "Odd" if the number is odd.
+// Example:
+// Input: isEvenOrOdd(10)
+// Output: 'Even'
+
+function isEvenOrOdd(number){
+    if(number % 2 === 0){
+        return "Even"
+    }
+
+    else{
+        return "Odd"
+    }
+}
+
+console.log(isEvenOrOdd(10));
