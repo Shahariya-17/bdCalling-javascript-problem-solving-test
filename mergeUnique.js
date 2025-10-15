e8e82bc6cb351187e155855a3782076cb9675f42
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-10-15 09:34:42 +0600

    added marge unique

--- mergeUnique.js
+++ mergeUnique.js
@@ -4,3 +4,9 @@
 // Input: mergeUnique([1, 2, 3], [3, 4, 5])
 // Output: [1, 2, 3, 4, 5]
 
+function mergeUnique(arr1, arr2) {
+
+  return [...new Set(arr1.concat(arr2))].sort((a, b) => a - b);
+}
+
+console.log(mergeUnique([1, 2, 3], [3, 4, 5]));
